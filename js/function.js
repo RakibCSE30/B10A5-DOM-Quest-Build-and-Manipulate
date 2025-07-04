1281667210f182860b0928c1be4bd572f8d35204
rakibul <rakibul1810130@gmail.com>
2025-07-05 01:01:58 +0600

    Add history page

--- js/function.js
+++ js/function.js
@@ -10,3 +10,19 @@ function getInnerText(id){
     const text=parseFloat(innerText);
     return text;
 }
+
+// Show modal
+function showModal(id) {
+  document.getElementById(id).classList.remove('hidden');
+}
+
+// Hide modal
+function hideModal(id) {
+  document.getElementById(id).classList.add('hidden');
+}
+
+
+function showModal() {
+  document.getElementById('donation-modal').checked = true;
+}
+
--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,50 @@
+const { createElement } = require("react");
 
-console.log('Heloo ja');
+console.log("Heloo ja");
 
-document.getElementById('donate-btn').addEventListener('click',function(event){
+document
+  .getElementById("donate-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
     // const amount=document.getElementById('input-amount').value;
-    const amount =getValue('input-amount');
+    const amount = getValue("input-amount");
+
+    if (isNaN(amount) || amount <= 0) {
+      alert("❌ Please enter a valid donation amount.");
+    //   hideModal('donate-btn');
+      return;
+    }
     // currentAmount=parseFloat(amount);
-    const mainBalance=getInnerText('main-balance');
-    console.log(amount ,mainBalance);
+    const mainBalance = getInnerText("main-balance");
+    console.log(amount, mainBalance);
+
+    const p= createElement('p');
+    p.innerText=`Added: ${amount} Tk New balance ${mainBalance}`;
+    console.log(p);
+    
+    document.getElementById('his-id').appendChild(p);
     
 
-
-    
     // const balance=document.getElementById('main-balance').innerText;
     // mainBalance=parseFloat(balance);
     // const newBalance=amount+mainBalance;
     // console.log(amount,typeof(amount),typeof(currentAmount),balance ,newBalance);
-    document.getElementById('main-balance').innerText=amount+mainBalance;
+    document.getElementById("main-balance").innerText = amount + mainBalance;
+  });
+
+
+
+
+//   history button
+document.getElementById('history-btn').addEventListener('click',function(){
+    console.log('history buuu');
+    window.location.href='/history.html';
+
+
     
+
     
 })
+
+
+ 
